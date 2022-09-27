afb447392a4e615795ff6a64d77b4753b48be579
Faruque Reza <faruque@ptssolutions.com>
2022-09-28 00:01:00 +0600

    ViewChild & ViewChildren

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -1,4 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  OnInit,
+  QueryList,
+  ViewChild,
+  ViewChildren,
+} from '@angular/core';
+import { NgModel } from '@angular/forms';
 
 import { IProduct } from './product';
 import { ProductService } from './product.service';
@@ -7,14 +16,22 @@ import { ProductService } from './product.service';
   templateUrl: './product-list.component.html',
   styleUrls: ['./product-list.component.css'],
 })
-export class ProductListComponent implements OnInit {
+export class ProductListComponent implements OnInit, AfterViewInit {
   pageTitle = 'Product List';
   // listFilter = '';
+  filtername = '';
   showImage = false;
 
   imageWidth = 50;
   imageMargin = 2;
   errorMessage = '';
+
+  @ViewChild('filterElement') filterElementref!: ElementRef;
+  // @ViewChildren('filterElement, nameElement')
+  // inputElementref!: QueryList<ElementRef>;
+
+  @ViewChildren(NgModel)
+  inputElementref!: QueryList<ElementRef>;
 
   //== Getter Setter >>>
   private _listFilter!: string;
@@ -32,6 +49,12 @@ export class ProductListComponent implements OnInit {
 
   constructor(private productService: ProductService) {}
 
+  ngAfterViewInit(): void {
+    this.filterElementref.nativeElement.focus();
+    //console.log(this.filterElementref);
+    console.log(this.inputElementref);
+  }
+
   ngOnInit(): void {
     this.productService.getProducts().subscribe({
       next: (products) => {
@@ -41,13 +64,6 @@ export class ProductListComponent implements OnInit {
       error: (err) => (this.errorMessage = err),
     });
   }
-
-  // The Long Way
-
-  // onFilterChange(filter: string): void {
-  //   this.listFilter = filter;
-  //   this.performFilter(this.listFilter);
-  // }
 
   toggleImage(): void {
     this.showImage = !this.showImage;
